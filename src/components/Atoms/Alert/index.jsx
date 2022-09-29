4160ba754dfdedc198f2904b884c02bef1303a0f
caramelribon <caramelribon137@gmail.com>
2022-09-29 17:45:11 +0900

    Alertの作成
    ・classをstyledの中に入れる
    
    .fadeInと.fadeOutのクラスをわかりやすくするためAlert/index.jsxの中の、一番外の要素StyledAlertを作ってその中に入れ子としていれました
    
    それにともない./style.cssのfadeInとfadeOutと, Alert/index.jsxでimportしていた所を削除しました

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -1,27 +1,44 @@
 import React from "react";
 import styled from "styled-components";
-import "../../../style.css";
 import COLOR from "../../../variables/color";
 import TEXT from "../../../variables/texts";
 import BREAKPOINT from "../../../variables/breakpoint";
 
 const Alert = ({ visible, errorText }) => {
   return (
-    <div className={visible ? "fadeIn" : "fadeOut"}>
-      <StyledAlertText>{errorText}</StyledAlertText>
-    </div>
+    <StyledAlert>
+      <StyledAlertText className={visible ? "fadeIn" : "fadeOut"}>
+        {errorText}
+      </StyledAlertText>
+    </StyledAlert>
   );
 };
 
 export default Alert;
 
-const StyledAlertText = styled.p`
+const StyledAlert = styled.div`
   position: absolute;
   top: 80px;
   left: 50%;
   transform: translateX(-50%);
   -webkit-transform: translateX(-50%);
   -ms-transform: translateX(-50%);
+  @media (max-width: ${BREAKPOINT.MEDIUM}) {
+    top: 40px;
+  }
+
+  .fadeIn {
+    transform: translateY(0);
+    opacity: 1;
+  }
+
+  .fadeOut {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+`;
+
+const StyledAlertText = styled.p`
   width: 400px;
   height: 40px;
   padding: 10px 20px;
@@ -30,8 +47,8 @@ const StyledAlertText = styled.p`
   color: ${COLOR.WHITE};
   ${TEXT.S};
   z-index: 1;
+  transition: all 0.5s ease 0s;
   @media (max-width: ${BREAKPOINT.MEDIUM}) {
-    top: 20px;
     width: 280px;
   }
 `;
